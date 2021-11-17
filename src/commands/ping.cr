cc509de56a8eca1755e4071c77b64414df2716e1
devnote-dev <pokeraven4@gmail.com>
2021-11-17 20:07:44 +0000

    feat(cmd): update ping command

--- src/commands/ping.cr
+++ src/commands/ping.cr
@@ -5,6 +5,11 @@ command = Command.new(
   "Sends the bot ping",
   "ping",
   0
-) do |client, ctx|
-  client.create_message ctx.channel_id, "pong!"
+) do |client, message|
+  msg = client.create_message message.channel_id, "pong!"
+  time = Time.utc - message.timestamp
+  client.edit_message(
+    message.channel_id, msg.id,
+    "API: #{time.total_milliseconds}ms"
+  )
 end
