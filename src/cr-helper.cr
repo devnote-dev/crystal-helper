f46f9c2a42d6dcbc1e481758fabff68d5af67798
devnote-dev <inferno0745@gmail.com>
2022-02-13 13:40:26 +0000

    feat: minor code cleanup

--- src/cr-helper.cr
+++ src/cr-helper.cr
@@ -3,16 +3,16 @@ require "yaml"
 
 require "./commands/ping.cr"
 
-config = File.open("./config.yml") do |file|
-  YAML.parse file
-end
+config = File.open("./config.yml") { |f| YAML.parse f }
 
 bot = Discord::Client.new(config["token"].as_s)
 
 bot.on_message_create do |message|
-	next if !message.content.starts_with? "$"
+	next unless message.content.starts_with? "$"
 
 	cmd = message.content.byte_slice(1).split(separator: " ", remove_empty: true)[0]
+	next unless cmd.size
+
 	case cmd
 	when "ping"
 		cmd_ping bot, message
